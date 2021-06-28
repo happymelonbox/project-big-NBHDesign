b4295a6327ccd7d8396fd3f901dd7962749b03a5
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-28 21:42:01 +1000

    working on contact form with javascript

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,36 +1,15 @@
 <?php
-
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-
-
-$myemail = 'howes.j.j@gmail.com';
-
-$name = $_GET['name'];
-
-$email_address = $_GET['email'];
-
-$message = $_GET['message'];
-
-$name = filter_var($name, FILTER_SANITIZE_STRING);
-
-$email_address = str_replace(array("\r", "\n", "%0a", "%0d"), '', $email_address);
-$email_address = filter_var($email_address, FILTER_VALIDATE_EMAIL);
-
-$message = filter_var($message, FILTER_SANITIZE_STRING);
-
-    $from = $myemail;
-    $to = $myemail;
-    $subject = "New contact from " . $name;
-    $message = "Message: " .$message . "Contact email: " .$email_address;
-    $headers = "From: " . $email_address;
-
-    mail($to,$subject,$message, $headers);
-    if(mail($to,$subject,$message, $headers)){
-        echo "success!";
-        } else {
-            echo "failed!";
-        };
-        header('Location: /index.html')
-
+if (isset($_POST)) {
+    $to = "howes.j.j@gmail.com";
+    $subject = 'New enquiry';
+    $data = json_decode(file_get_contents('php://input'), true);
+    $message = 'Name: '.$data['name'].' Email: '.$data['emailInput'.' Message: '.$data['message']];
+    $success = mail($to, $subject, $message);
+    if ($success) {
+        echo "Success!";
+    }
+    else {
+        echo "Fail";
+    }
+}
 ?>
--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ function loadPage(){
     navButtonClick()
     gitBut()
     fetchInsta()
-    // instaFeed()
-    
-    // submitForm()
-
+    submitForm()
+}
 function navButtonClick(){
     let navButton = document.querySelectorAll('a.navButton')
     for(let i=0;i<navButton.length;i++){
@@ -86,19 +84,18 @@ function displayInsta(feed){
 
 function submitForm(){
     const contactForm = document.getElementById('send')
-
     contactForm.addEventListener('submit', function(event){
         event.preventDefault()
-
-        const formData = new FormData(this)
-
-        fetch("/assets/php/contact.php",{
-            method: 'POST',
-            body: formData
+        const data = JSON.stringify({ name: this.state.name, email: this.state.emailInput, message: this.state.message })
+        fetch('/assets/contact.php', {
+          method: 'POST',
+          body: data
+        }).then((response) => {
+          if (response.ok) {
+            this.setState({ success: true })
+          }
         })
-        .then(resp=>resp.text())
-        .then(text=>console.log(text))
-        .catch(error=>console.error(error))
     })
 }
-}
+
+
